1e93b8dc8fc4033cd46a10dfee00257a6fd1a910
tin1573-cmis <tin1573@cmis.ac.th>
2015-08-18 09:46:18 +0700

    Created FundamentalsP1

---
+++ FundamentalsP1/FundamentalsP1.java
@@ -0,0 +1,11 @@
+
+public class FundamentalsP1 
+    {
+  public static void main ( String args[] )
+  {
+    System.out.println("My favorite food is Thai food.");
+    System.out.println("My favorite color is Green.");
+    System.out.println("My favorite movie is Blended.");
+    System.out.println("My favorite musical group is The Beatles.");
+      } // end main
+} // end FundamentalsP1
